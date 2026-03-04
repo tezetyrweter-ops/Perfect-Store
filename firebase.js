80c48d8995c9910f6ce953468a8af82bc030efd1
tezetyrweter-ops <tezetyrweter@gmail.com>
2026-03-04 23:40:44 +0100

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC2euiKINjcx9Ay8qapBMCDbh9QFVzsVss",
+  authDomain: "abdo-eea3d.firebaseapp.com",
+  databaseURL: "https://abdo-eea3d-default-rtdb.firebaseio.com",
+  projectId: "abdo-eea3d",
+  storageBucket: "abdo-eea3d.firebasestorage.app",
+  messagingSenderId: "516114812388",
+  appId: "1:516114812388:web:a16dc18f4abe3"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
